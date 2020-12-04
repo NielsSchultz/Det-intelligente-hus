0ec98de50e5bb1a678cfb72fd3646fb89b00aa67
NielsSchultz <53754565+NielsSchultz@users.noreply.github.com>
2020-12-04 08:35:36 +0100

    test

---
+++ Det intelligente hus/main.c
@@ -0,0 +1,18 @@
+/*
+ * Det intelligente hus.c
+ *
+ * Created: 04-12-2020 08:25:15
+ * Author : Niels
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
